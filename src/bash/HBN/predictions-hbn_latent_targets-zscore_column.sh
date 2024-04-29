b8e8d526260b9550acb2cab6e09fe757c28764e2
JessyD <jejedafflon@gmail.com>
2024-04-29 17:49:52 -0400

    Add scripts to run analyses

--- src/bash/HBN/predictions-hbn_latent_targets-zscore_column.sh
+++ src/bash/HBN/predictions-hbn_latent_targets-zscore_column.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Note: before running this script, please activate the virtual environment. You
-# can do that by using:
-# conda activate bblocks-phenotype
-
 # Path to where the code is. Change accordingly
 project_dir="/home/jdafflon/brain-phenotypes"
 phenotype_dir="/Datasets/BrainBlocks/DerivativeDatasets/HBN/brainblocks-0.1.0/phenotype"
-config_file="${project_dir}/config/HBN/predictions-hbn-latent_targets_svd-reconstructions-zscore_column_y_val_orig.yaml"
-y_val="orig"
-experiment_name="regress_covariates_predictions-hbn-latent_targets_svd-zscore_y_val_${y_val}_column-029_component"
 figs_dir="${project_dir}/figs/HBN"
+config_file="${project_dir}/config/HBN/predictions-hbn-latent_targets-zscore_column.yaml"
+experiment_name="regress_covariates_predictions-hbn-latent_targets-zscore_column"
 output_dir="${project_dir}/outputs/HBN/${experiment_name}"
-fc_dir="/Datasets/BrainBlocks/DerivativeDatasets/HBN/brainblocks-0.1.0/"
+fc_dir="${project_dir}/data/HBN/brainblocks-0.1.0"
 predictions_dir="/Datasets/BrainBlocks/DerivativeDatasets/HBN/brainblocks-0.1.0/prediction_experiments/predict-PHENOTYPE+age_at_scan+family_ID+sex_at_birth-from_task-rest_atlas-Schaefer2018400Parcels17Networksorder_space-MNI_desc-clean_bold_conndata-network_connectivity"
 n_components=29
 
@@ -20,12 +15,12 @@ n_components=29
 python ${project_dir}/src/fc_prediction.py \
 	--config_file=${config_file} \
 	--figs_dir=${output_dir} \
+	--output_dir=${output_dir} \
 	--project_dir=${project_dir} \
-	--experiment_name=${experiment_name} \
 	--fc_dir=${fc_dir} \
 	--predictions_dir=${predictions_dir} \
-	--output_dir=${output_dir} \
-    --phenotype_dir=${phenotype_dir} \
+	--phenotype_dir=${phenotype_dir} \
+	--experiment_name=${experiment_name} \
     --regress_out_covariates \
     --n_components=${n_components}
 
--- src/bash/HBN/predictions-hbn_targets-zscore_column_covariate_regression.sh
+++ src/bash/HBN/predictions-hbn_targets-zscore_column_covariate_regression.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
-# Note: before running this script, please activate the virtual environment. You
-# can do that by using:
-# conda activate bblocks-phenotype
-
 # Path to where the code is. Change accordingly
 project_dir="/home/jdafflon/brain-phenotypes"
 phenotype_dir="/Datasets/BrainBlocks/DerivativeDatasets/HBN/brainblocks-0.1.0/phenotype"
-config_file="${project_dir}/config/HBN/predictions-hbn-latent_targets_svd-reconstructions-zscore_column_y_val_orig.yaml"
-y_val="orig"
-experiment_name="regress_covariates_predictions-hbn-latent_targets_svd-zscore_y_val_${y_val}_column-029_component"
 figs_dir="${project_dir}/figs/HBN"
+config_file="${project_dir}/config/HBN/predictions-hbn-targets-latent_None-zscore_column.yaml"
+experiment_name="regress_covariates_predictions-hbn_test"
 output_dir="${project_dir}/outputs/HBN/${experiment_name}"
-fc_dir="/Datasets/BrainBlocks/DerivativeDatasets/HBN/brainblocks-0.1.0/"
+fc_dir="${project_dir}/data/HBN/brainblocks-0.1.0"
 predictions_dir="/Datasets/BrainBlocks/DerivativeDatasets/HBN/brainblocks-0.1.0/prediction_experiments/predict-PHENOTYPE+age_at_scan+family_ID+sex_at_birth-from_task-rest_atlas-Schaefer2018400Parcels17Networksorder_space-MNI_desc-clean_bold_conndata-network_connectivity"
-n_components=29
 
 # run script
 python ${project_dir}/src/fc_prediction.py \
 	--config_file=${config_file} \
 	--figs_dir=${output_dir} \
+	--output_dir=${output_dir} \
 	--project_dir=${project_dir} \
-	--experiment_name=${experiment_name} \
 	--fc_dir=${fc_dir} \
 	--predictions_dir=${predictions_dir} \
-	--output_dir=${output_dir} \
-    --phenotype_dir=${phenotype_dir} \
-    --regress_out_covariates \
-    --n_components=${n_components}
-
+	--phenotype_dir=${phenotype_dir} \
+	--experiment_name=${experiment_name} \
+    --regress_out_covariates
