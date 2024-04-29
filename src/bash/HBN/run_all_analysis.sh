78090415eec6c6e87b4ed6a5296218e7be406a90
JessyD <jejedafflon@gmail.com>
2024-04-29 13:58:43 -0400

    Add bash scripts for latent analysis

---
+++ src/bash/HBN/run_all_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#./predictions-hbn_latent_targets-zscore_column.sh
+
+# Reconstruction
+./predictions-hbn-latent_targets_svd-zscore_column-1.sh
+./predictions-hbn-latent_targets_svd-zscore_column-2.sh
+./predictions-hbn-latent_targets_svd-zscore_column-3.sh
+./predictions-hbn-latent_targets_svd-zscore_column-4.sh
+./predictions-hbn-latent_targets_svd-zscore_column-5.sh
+
+# Reliability
